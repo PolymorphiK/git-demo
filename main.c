e2b6509fbb62d1f849b6473e645acb98760e27e2
Kevin Pacheco <k.pacheco@me.com>
2020-04-13 13:45:20 -0700

    Added some comments...

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 
 int main(int argc, char** argv) {
     printf("Hello, World\n");
-    
+
     return EXIT_SUCCESS;
 }
+
+// Github
+// master
+// c0 (readme)
+// end...
+
+// local
+// master
+// c0 (readme) -> c1 (main.c)
