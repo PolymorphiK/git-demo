d1015e12274287ea6475df87a83c9f94e0a50031
Kevin Pacheco <k.pacheco@me.com>
2020-04-13 13:57:35 -0700

    fixes #1

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
+    for(int i = 0; i < argc; ++i) {
+        printf("%i %s\n", i, argv[0]);
+    }
+
     printf("Hello, World\n");
 
     return EXIT_SUCCESS;
